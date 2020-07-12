c1849af3b1ce1c66cf425d45c4e1143c4359ca1e
Konstantin Tenman <konstantin.tenman@monese.com>
2020-07-12 15:50:05 +0100

    initial commit

---
+++ src/main/java/com/edozo/java/test/controllers/MapDto.java
@@ -0,0 +1,29 @@
+package com.edozo.java.test.controllers;
+
+import java.util.Objects;
+
+public class MapDto {
+
+    private final int id;
+
+    public MapDto(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MapDto mapDto = (MapDto) o;
+        return id == mapDto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
